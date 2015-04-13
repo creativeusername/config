79d209f4a630d52f310c456f432d41112028df8a
Maximilian Kerp <maxkerp@gmail.com>
2015-04-13 20:54:55 +0200

    Add files and configs

---
+++ bash_aliases
@@ -0,0 +1,20 @@
+######################################################################
+#
+# Useful aliases i found over the years.
+#
+# Author: Maximilian Kerp
+# Date: April 13th, 2015
+#
+######################################################################
+
+# Useful Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias gopen='gnome-open'
+alias dirs="ls -1d */"
+alias files="ls -p | grep -v /"
+alias ll='ls -alF'
+alias la='ls -AF'
+alias l='ls -l'
+alias cl='clear'
+alias agrep='la | grep'
---
+++ provision.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Folders
+rm -rf ~/Documents
+rm -rf ~/Public
+rm -rf ~/Templates
+rm -rf ~/Videos
+rm -rf ~/Music
+mkdir ~/College
+mkdir ~/Git
+mkdir ~/Projects
+
+# Adding needed PPAs to apt
+sudo add-apt-repository -y ppa:webupd8team/atom # Atom.io Text Editor
+sudo add-apt-repository -y "deb http://dl.google.com/linux/chrome/deb/ stable main" # Chrome Browser
+sudo add-apt-repository -y ppa:mpstark/elementary-tweaks-daily # Elementary Tweaks for 0.3 Freya
+
+# Basic update
+sudo apt-get -y --force-yes update
+sudo apt-get -y --force-yes upgrade
+sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily
+
+# Install apps. Comment out if not needed
+sudo apt-get -y install \
+    guake \
+    git \
+    curl \
+    elementary-tweaks \
+    google-chrome-stable \
+    # thinkfan \
+    atom
+
+# Configure System
+cd Git && git clone https://github.com/creativeusername/config.git && cd ~
+cp -f ~/Git/config/bash_aliases ~/.bash_aliases
+cp -f ~/Git/config/gitconfig ~/.gitconfig
+cp -f ~/Git/config/%gconf.xml ~/.gconf/apps/guake/genreal/%gconf.xml
+
+# Install Node Version Manager
+curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
+nvm install stable
+nvm alias default stable
+
+# Configure Node, Install all needed packages
+# TODO
+
+# Configure Atom
+apm install monokai
+# TODO create package list file
+
+# Thinkpad fan settings
+# http://hackmemory.wordpress.com/2012/07/19/lenovo-x200-tuning/
+# echo "tp_smapi" | sudo tee -a /etc/modules
+# echo "thinkpad_acpi" | sudo tee -a /etc/modules
+# echo "options thinkpad_acpi fan_control=1" | sudo tee /etc/modprobe.d/thinkpad_acpi.conf
+# sudo cp ./data/etc/default/thinkfan /etc/default/thinkfan
+# sudo cp ./data/etc/thinkfan.conf /etc/thinkfan.conf
+# sudo chmod 744 /etc/default/thinkfan
+# sudo chmod 744 /etc/thinkfan.conf
+
+# Prompt for a reboot
+clear
+echo ""
+echo "===================="
+echo " TIME FOR A REBOOT! "
+echo "===================="
+echo ""
